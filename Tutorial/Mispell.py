e75af674b47ff618f68a279f7d00fcd9a06fb7fa
Loading <bjhlvtna@gmail.com>
2015-12-22 00:02:54 +0900

    [ fix ] modify enumerate start index 1

--- Tutorial/Mispell.py
+++ Tutorial/Mispell.py
@@ -5,8 +5,8 @@ testCase = []
 for i in range(int(maxTestCaseNum)):
     testCase.append(input())
 
-for index, case in enumerate(testCase):
+for index, case in enumerate(testCase, 1):
     inputData = case.split()
     word = inputData[1]
-    print(str(int(index)+1) + ' ' + word[:int(inputData[0])-1]+word[int(inputData[0]):])
+    print(str(index) + ' ' + word[:int(inputData[0])-1]+word[int(inputData[0]):])
 
